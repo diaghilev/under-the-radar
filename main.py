70c068c15ebb61c8964aead21a316d181f2c60f7
Lauren <hi@laukaye.com>
2023-01-05 15:02:47 -0800

    Programmatically create dataset

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This script will access twitter API to pull tweets based on a query
+# This script ingests tweets from twitter API and loads them to BigQuery
 
 # import packages
 import tweepy
@@ -12,81 +12,75 @@ import pandas as pd
 import pprint
 import time
 
-# read configs
-config = configparser.ConfigParser()
-config.read('config.ini')
+# # read configs
+# config = configparser.ConfigParser()
+# config.read('config.ini')
 
-# set up authentication
-api_key = config['twitter']['api_key']
-api_key_secret = config['twitter']['api_key_secret']
-access_token = config['twitter']['access_token']
-access_token_secret = config['twitter']['access_token_secret']
+# # set up authentication
+# api_key = config['twitter']['api_key']
+# api_key_secret = config['twitter']['api_key_secret']
+# access_token = config['twitter']['access_token']
+# access_token_secret = config['twitter']['access_token_secret']
 
-client = tweepy.Client(
-   consumer_key= api_key,
-   consumer_secret= api_key_secret,
-   access_token= access_token,
-   access_token_secret= access_token_secret,
-   return_type = requests.Response #may be potential to just return a dict straight away
-)
+# client = tweepy.Client(
+#    consumer_key= api_key,
+#    consumer_secret= api_key_secret,
+#    access_token= access_token,
+#    access_token_secret= access_token_secret,
+#    return_type = requests.Response #may be potential to just return a dict straight away
+# )
 
-# define query
-query = 'analytics engineer #hiring' #query = 'context:66.961961812492148736 lang:en #hiring #remote data engineer'
+# # define query
+# query = 'analytics engineer #hiring' #query = 'context:66.961961812492148736 lang:en #hiring #remote data engineer'
 
-tweets = client.search_recent_tweets(
-   query=query,
-   #tweet_fields=['author_id','created_at','geo','lang','context_annotations'],
-   max_results=10,
-   user_auth=True
-)
+# tweets = client.search_recent_tweets(
+#    query=query,
+#    #tweet_fields=['author_id','created_at','geo','lang','context_annotations'],
+#    max_results=10,
+#    user_auth=True
+# )
 
-# convert result to dictionary
-tweets_dict_full = tweets.json() 
-tweets_dict = tweets_dict_full['data'] #extract "data" value from dict
-pprint.pprint(tweets_dict[0])
+# # convert result to dictionary
+# tweets_dict_full = tweets.json() 
+# tweets_dict = tweets_dict_full['data'] #extract "data" value from dict
 
-# write to a JSONL file
-with open("tweets.jsonl", "w") as f:
-   for line in tweets_dict:
-      f.write(json.dumps(line) + "\n")
+# # write to a JSONL file
+# with open("tweets.jsonl", "w") as f:
+#    for line in tweets_dict:
+#       f.write(json.dumps(line) + "\n")
 
 #####
 
-# import google cloud packages
-from google.cloud import bigquery
-from google.cloud.exceptions import NotFound
+def create_dataset():
 
-# Set google application credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/laurenkaye/PycharmProjects/tweets/apikey.json"
+   # Import google cloud packages
+   from google.cloud import bigquery
+   from google.cloud.exceptions import NotFound
 
-# Construct BigQuery client object
-client = bigquery.Client
+   # Set google application credentials
+   os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/laurenkaye/PycharmProjects/tweets/apikey.json"
 
-bq_project_id = 'modular-terra-372321'
-bq_dataset = 'tweets_dataset'
+   # Construct BigQuery client object
+   client = bigquery.Client() 
 
-exit()
+   # Set dataset_id to the ID of the dataset to create.
+   dataset_id = "{}.tweets_dataset".format(client.project)
 
-#(BigQuery official docs)
+   # Construct a full Dataset object to send to the API.
+   dataset = bigquery.Dataset(dataset_id)
 
-# Set dataset_id to the ID of the dataset to create.
-dataset_id = {}.bq_dataset.format(client.project)
+   # Specify the geographic location where the dataset should reside.
+   dataset.location = "US"
 
-# Construct a full Dataset object to send to the API.
-dataset = bigquery.Dataset(dataset_id)
+   # Send the dataset to the API for creation 
+   dataset = client.create_dataset(dataset, timeout=30)  # API request.
+   print("Created dataset")
 
-# Specify the geographic location where the dataset should reside.
-dataset.location = "US"
-
-# Send the dataset to the API for creation, with an explicit timeout.
-# Raises google.api_core.exceptions.Conflict if the Dataset already exists within the project.
-dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
-print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
-
+if __name__ == '__main__':
+    create_dataset()
 
 # create table in bigquery 
 # set schema
-
 # load file to BigQuery
 
 
