83af501c0e8590c913da57a4cfb982467a055aef
Lauren <hi@laukaye.com>
2023-01-24 17:15:32 -0800

    Added dim_jobs model

--- under_the_radar_dbt/models/src/src_slack_jobs.sql
+++ under_the_radar_dbt/models/src/src_slack_jobs.sql
@@ -1,11 +1,10 @@
 WITH raw_slack_jobs AS (
   SELECT * FROM {{ source('bigquery', 'raw_slack_jobs')}}
-  -- SELECT * FROM modular-terra-372321.tweets_dataset.raw_slack_jobs
 )
 SELECT
    {{ dbt_utils.generate_surrogate_key(
       ['text']
-  ) }} as slack_id,
+  ) }} as slack_id, -- generate primary key
   text as slack_text,
   url as slack_url,
   timestamp,
