7df27c1df2e2304f9741a131afda6c9a4f4e7d5a
Lauren <hi@laukaye.com>
2023-02-02 14:34:02 -0800

    Changed is_metric fields to boolean from string

--- dbt/models/mart/wkly_metrics.sql
+++ dbt/models/mart/wkly_metrics.sql
@@ -5,8 +5,8 @@ WITH dim_jobs AS (
 SELECT 
   DATE_TRUNC(timestamp, week) as week,
   COUNT(job_id) AS count_jobs,
-  ROUND(SUM(CASE WHEN is_contract = 'Yes' THEN 1 ELSE 0 END)/COUNT(job_id)*100,2) AS percent_contract, -- In our sample, do we see contract trends as a result of the economic downturn?
-  ROUND(SUM(CASE WHEN is_parttime = 'Yes' THEN 1 ELSE 0 END)/COUNT(job_id)*100,2) AS percent_parttime, -- In our sample, do we see parttime trends as a result of the economic downturn?
-  ROUND(SUM(CASE WHEN is_remote = 'Yes' THEN 1 ELSE 0 END)/COUNT(job_id)*100,2) AS percent_remote -- In our sample, do we see an increasing RTO trend?
+  ROUND(SUM(CASE WHEN is_contract = true THEN 1 ELSE 0 END)/COUNT(job_id)*100,2) AS percent_contract, -- In our sample, do we see contract trends as a result of the economic downturn?
+  ROUND(SUM(CASE WHEN is_parttime = true THEN 1 ELSE 0 END)/COUNT(job_id)*100,2) AS percent_parttime, -- In our sample, do we see parttime trends as a result of the economic downturn?
+  ROUND(SUM(CASE WHEN is_remote = true THEN 1 ELSE 0 END)/COUNT(job_id)*100,2) AS percent_remote -- In our sample, do we see an increasing RTO trend?
  FROM dim_jobs
  GROUP BY week
