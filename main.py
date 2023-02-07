6f7d2537129888b04af44dda58d903cd25045f3b
Lauren <hi@laukaye.com>
2023-02-06 17:02:25 -0800

    Added comments for clarity

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def load_table(table_name, dataset_name, filename):
 if __name__ == '__main__':
     
     # set twitter query (requires experimentation)
-    query = '("BI developer" OR "BI engineer" OR "ETL" OR "ELT" OR "data engineer" -senior -lead -sr OR "Business Intelligence" OR Analytics) (interim OR #interim OR contractor OR #contractor OR contract OR #contract OR freelance OR #freelance OR #freelancer OR parttime OR part-time OR "part time" OR #parttime OR #part-time OR flexible OR #flexible OR months OR hours) (context:131.1197909704803901440 OR #hiring) -is:retweet -Full-Time -instructor'
+    query = '("BI developer" OR "BI engineer" OR "ETL" OR "ELT" OR "data engineer" -senior -lead -sr OR "Business Intelligence" OR Analytics) (interim OR #interim OR contractor OR #contractor OR contract OR #contract OR freelance OR #freelance OR #freelancer OR parttime OR part-time OR "part time" OR #parttime OR #part-time OR flexible OR #flexible OR months OR hours) (context:131.1197909704803901440 OR #hiring) -is:retweet'
     
     # set data landing locations
     filename = 'tweets.jsonl'
