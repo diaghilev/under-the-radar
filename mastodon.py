151cc7653514b24d9fcbfcf8f982336ab5781f42
Lauren <hi@laukaye.com>
2023-02-09 17:00:16 -0800

    Successful: Extract Mastodon post and Load to JSONL

--- mastodon.py
+++ mastodon.py
@@ -1,26 +1,65 @@
-# Extract statuses from Mastodon
-### WORK IN PROGRESS. Currently evaluating alternative data sources
+# This script extracts posts (officially called 'statuses' or 'toots') from the Mastodon API and loads them to Bigquery
 
+# import packages
 import requests
+import json
+from pprint import pprint
+from bs4 import BeautifulSoup
 
-auth = {'Authorization': 'Bearer ExAh3LOJC0T9R2ivYQYIBdVeaNg3tQkkbS5s30g-nyU'}
-url = 'https://data-folks.masto.host/api/v2/search' # Mastodon API endpoint for search
-params = {'q': '#hiring data'} # Set keywords for the search
+# create function to get statuses
+def get_posts():
 
-# Make GET request to the API endpoint
-response = requests.get(url, data=params, headers=auth)
+    # Make GET request to the API endpoint
+    auth = {'Authorization': 'Bearer ExAh3LOJC0T9R2ivYQYIBdVeaNg3tQkkbS5s30g-nyU'}
+    url = 'https://data-folks.masto.host//api/v1/timelines/tag/:hiring' # API endpoint
+    params = {'all':['data'], 'limit': 2}
 
-# Check the response status code
-if response.status_code == 200:
-    # Get the JSON data from the response
-    data = response.json()
-    #print(data)
+    response = requests.get(url, data=params, headers=auth)
 
-    # Print the statuses that contain the keywords
-    for status in data["statuses"]:
-        print(status["content"])
-else:
-    print("Request failed with status code:", response.status_code)
+    # Check the response status code
+    if response.status_code == 200:
+        
+        # Convert response to JSON
+        data = response.json()
+
+        #Extract statuses from the JSON
+        posts = []
+
+        for idx, item in enumerate(data):
+            id = data[idx]['id']
+            url = data[idx]['url']
+            created_at = data[idx]['created_at']
+            content_raw = data[idx]['content']
+            acct = data[idx]['account']['acct']
+
+            # Remove HTML from text
+            soup = BeautifulSoup(content_raw,'html.parser')
+            content = soup.get_text()
+
+            post = {
+            'id': id,
+            'created_at': created_at,
+            'content': content,
+            'url': url,
+            'acct': acct
+            }
+
+            posts.append(post)
+
+        return posts
+
+    else:
+        print("Request failed with status code:", response.status_code)
+
+# write result to a JSONL file
+filename = 'mastodon.jsonl'
+   
+with open(filename, "w") as f:
+    for line in get_posts():
+        f.write(json.dumps(line) + "\n") 
+
+print("File updated: {}".format(filename))
+
 
 
 
