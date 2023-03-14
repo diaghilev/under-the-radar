74b6b436bd88d16c3472186bceb1c229116b40a5
Lauren <hi@laukaye.com>
2023-03-13 18:48:19 -0700

    Type hints and comment improvements

--- twitter.py
+++ twitter.py
@@ -1,14 +1,14 @@
 """
-In summary, this script extracts tweets from the Twitter API and loads them to Bigquery.
+In summary, this script extracts tweets from the Twitter API and loads them to a Bigquery table.
 
 This script runs the following functions:
    get_tweets - Get list of Tweet objects from the Twitter API based on a search query
-   to_file - Load Tweets to a JSONL file
-   create_dataset - Create a BigQuery Dataset if it does not exist
-   create_table - Create a BigQuery Table if it does not exist
-   load_table - Load the BigQuery Table from the JSONL file
+   write_tweets_to_jsonl - Load Tweets to a JSONL file
+   create_bigquery_dataset - Create a BigQuery Dataset if it does not exist
+   create_bigquery_table - Create a BigQuery Table if it does not exist
+   load_jsonl_to_table - Load the BigQuery Table from the JSONL file
 
-Last Updated: 2023-02
+Last Updated: 2023-03
 """
 
 # import packages
@@ -27,7 +27,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS']='/Users/laurenkaye/PycharmProjects/
 # Construct BigQuery client object
 CLIENT = bigquery.Client() 
 
-# create function to get tweets
+# Create function to get tweets
 def get_tweets(query: str) -> list[dict]:
    """Get a list of Tweet objects from the Twitter API
     
@@ -37,35 +37,35 @@ def get_tweets(query: str) -> list[dict]:
    Returns:
         A list of Tweet objects, which are not yet processed
    """
-   # read configs
-   config = configparser.ConfigParser()
+   # Read configs
+   config: configparser.ConfigParser = configparser.ConfigParser()
    config.read('config.ini')
 
-   # set up authentication
-   api_key = config['twitter']['api_key']
-   api_key_secret = config['twitter']['api_key_secret']
-   access_token = config['twitter']['access_token']
-   access_token_secret = config['twitter']['access_token_secret']
+   # Set up authentication
+   api_key: str = config['twitter']['api_key']
+   api_key_secret: str = config['twitter']['api_key_secret']
+   access_token: str = config['twitter']['access_token']
+   access_token_secret: str = config['twitter']['access_token_secret']
 
    tweepy_client = tweepy.Client(
-      consumer_key= api_key,
-      consumer_secret= api_key_secret,
-      access_token= access_token,
-      access_token_secret= access_token_secret,
+      consumer_key = api_key,
+      consumer_secret = api_key_secret,
+      access_token = access_token,
+      access_token_secret = access_token_secret,
       return_type = requests.Response 
    )
 
    tweets_raw = tweepy_client.search_recent_tweets(
       query=query,
-      tweet_fields=["id","text","created_at"], 
+      tweet_fields=['id','text','created_at'], 
       max_results=10,
       user_auth=True
    )
 
    return tweets_raw
 
-# create function to put tweets in jsonl file 
-def to_file(filename: str, query: str) -> None:
+# Create function to write tweets to jsonl file 
+def write_tweets_to_jsonl(filename: str, query: str) -> None:
    """Load Tweet objects to a JSONL file
     
    Args:
@@ -73,20 +73,20 @@ def to_file(filename: str, query: str) -> None:
         query: The search query to use to get tweets
             
    """ 
-   # convert tweets to dictionary and remove metadata
+   # Convert tweets to dictionary and remove metadata
    tweets_raw = get_tweets(query) #datatype <class 'requests.models.Response'>
    tweets_dict = tweets_raw.json() #datatype <class 'dict'>
    tweets = tweets_dict['data'] #datatype <class 'list'>
 
-   # write result to a JSONL file
-   with open(filename, "w") as f:
+   # Write result to a JSONL file
+   with open(filename, 'w') as f:
       for line in tweets:
-         f.write(json.dumps(line) + "\n") 
+         f.write(json.dumps(line) + '\n') 
    
-   print(f"File updated: {filename}")
+   print(f'File updated: {filename}')
 
-# Create dataset if none exists
-def create_dataset(dataset_name: str) -> bigquery.Dataset:
+# Create function to create BigQuery dataset if none exists
+def create_bigquery_dataset(dataset_name: str) -> bigquery.Dataset:
    """Create the BigQuery Dataset if it does not already exist
     
     Args:
@@ -96,43 +96,43 @@ def create_dataset(dataset_name: str) -> bigquery.Dataset:
         A Dataset Object
     """
    # Set dataset_id to the ID of the dataset to create.
-   dataset_id = f"{CLIENT.project}.{dataset_name}"
+   dataset_id: str = f'{CLIENT.project}.{dataset_name}'
 
-   # Construct a Dataset object to send to the API.
-   dataset = bigquery.Dataset(dataset_id)
-   dataset.location = "US"
+   # Construct a BigQuery Dataset object to send to the API.
+   dataset: bigquery.Dataset = bigquery.Dataset(dataset_id)
+   dataset.location: str = 'US'
 
-   # Create dataset if none exists
+   # Create BigQuery dataset if none exists
    try:
         CLIENT.get_dataset(dataset_id) 
    except NotFound:
-        dataset = CLIENT.create_dataset(dataset, timeout=30)  
-        print(f"Dataset created: {dataset_id}")
+        dataset: bigquery.Dataset = CLIENT.create_dataset(dataset, timeout=30)  
+        print(f'Dataset created: {dataset_id}')
 
    return dataset
 
-# Create table if none exists
-def create_table(table_name: str, dataset_name: str):
+# Create function to create BigQuery table if none exists
+def create_bigquery_table(table_name: str, dataset_name: str) -> bigquery.Table:
    """Create the BigQuery Table if it does not already exist
     
     Args:
         table_name: The name of the table to create
         dataset_name: The name of the dataset containing the table
    """
-   # Construct table reference
-   dataset = create_dataset(dataset_name)
-   table_id = dataset.table(table_name)
-   table = bigquery.Table(table_id)
+   # Construct BigQuery table reference
+   dataset: bigquery.Dataset = create_bigquery_dataset(dataset_name)
+   table_id: bigquery.TableReference = dataset.table(table_name)
+   table: bigquery.Table = bigquery.Table(table_id)
 
-   # Check if table exists. If not, create table.
+   # Check if BigQuery table exists. If not, create table.
    try:
       CLIENT.get_table(table) #API request
    except NotFound:
-      table = CLIENT.create_table(table)
-      print(f"Table created: {table}")
+      table: bigquery.Table = CLIENT.create_table(table)
+      print(f'Table created: {table}')
 
-# Load table from JSONL
-def load_table(table_name: str, dataset_name: str, filename: str):
+# Create function to load BigQuery table from JSONL
+def load_jsonl_to_table(table_name: str, dataset_name: str, filename: str) -> None:
    """Load Tweets in the JSONL file to the BigQuery Table
     
     Args:
@@ -140,44 +140,44 @@ def load_table(table_name: str, dataset_name: str, filename: str):
         table_name: The name of the table to create
         filename: JSONL file containing the tweets
     """
-   # Construct table reference
-   dataset = create_dataset(dataset_name)
-   table_id = dataset.table(table_name)
+   # Construct BigQuery table reference
+   dataset: bigquery.Dataset = create_bigquery_dataset(dataset_name)
+   table_id: bigquery.TableReference = dataset.table(table_name)
 
-   # Define table schema
+   # Define BigQuery table schema
    job_config = bigquery.LoadJobConfig(
       autodetect=True,
       source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
       write_disposition='WRITE_TRUNCATE' # {WRITE_APPEND; WRITE_EMPTY}
    )
 
-   # Upload JSONL to BigQuery
-   with open(filename, "rb") as file:
+   # Upload JSONL to BigQuery table
+   with open(filename, 'rb') as file:
       job = CLIENT.load_table_from_file(file, table_id, job_config=job_config)
 
    while job.state != 'DONE':
       job.reload()
       time.sleep(2)
-   print(f"Job completed: {job.result()}")
+   print(f'Job completed: {job.result()}')
 
 if __name__ == '__main__':
     
-    # set twitter query (requires experimentation)
+    # Set query that defines tweet search query. (requires experimentation)
     #query = '("BI developer" OR "BI engineer" OR "ETL" OR "ELT" OR "data engineer" -senior -lead -sr OR "Business Intelligence" OR Analytics) (interim OR #interim OR contractor OR #contractor OR contract OR #contract OR freelance OR #freelance OR #freelancer OR parttime OR part-time OR "part time" OR #parttime OR #part-time OR flexible OR #flexible OR months OR hours) (context:131.1197909704803901440 OR #hiring) -is:retweet'
-    query = '"analytics engineer" #hiring -is:retweet'
+    query: str = '"analytics engineer" #hiring -is:retweet'
   
 
-    # set data landing locations
-    filename = 'tweets.jsonl'
-    dataset_name = 'tweets_dataset' 
-    table_name = 'raw_twitter_jobs'
+    # Set landing locations for data from the Twitter API
+    filename: str = 'tweets.jsonl'
+    dataset_name: str = 'tweets_dataset' 
+    table_name: str = 'raw_twitter_jobs'
 
-   #finally, run functions
+   # Run functions that extract tweets from the Twitter API and loads them to Bigquery
     get_tweets(query)
-    to_file(filename, query)
-    create_dataset(dataset_name)
-    create_table(table_name, dataset_name)
-    load_table(table_name, dataset_name, filename)
+    write_tweets_to_jsonl(filename, query)
+    create_bigquery_dataset(dataset_name)
+    create_bigquery_table(table_name, dataset_name)
+    load_jsonl_to_table(table_name, dataset_name, filename)
 
 
 
